086c950623f4d96f11d6eccb877d915f38b860c1
sadeep <sadeep@surecore.co>
2021-12-04 16:55:49 +0530

    Initial commit

---
+++ src/main/java/lk/lak_derana_hms/LakDeranaHmsApplication.java
@@ -0,0 +1,13 @@
+package lk.lak_derana_hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakDeranaHmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LakDeranaHmsApplication.class, args);
+    }
+
+}
