2288f7f10311314e2e3d22441e84bdfb0cd0661c
sadeep <sadeep@surecore.co>
2021-12-12 22:43:09 +0530

    Added customize error response.

--- src/main/java/lk/lakderana/hms/security/SecurityConfig.java
+++ src/main/java/lk/lakderana/hms/security/SecurityConfig.java
@@ -3,6 +3,9 @@ package lk.lakderana.hms.security;
 import lk.lakderana.hms.security.filter.CustomAuthenticationFilter;
 import lk.lakderana.hms.security.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +17,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
@@ -47,5 +55,42 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    private static final String DEFAULT_KEY_TIMESTAMP = "timestamp";
+    private static final String DEFAULT_KEY_STATUS = "status";
+    private static final String DEFAULT_KEY_ERROR = "error";
+    private static final String DEFAULT_KEY_ERRORS = "errors";
+    private static final String DEFAULT_KEY_MESSAGE = "message";
+    private static final String DEFAULT_KEY_PATH = "path";
+
+    public static final String KEY_STATUS = "status";
+    public static final String KEY_CODE = "code";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_ERROR = "error";
+    public static final String KEY_MESSAGE = "message";
+    public static final String KEY_TIMESTAMP = "timestamp";
+    public static final String KEY_ERRORS = "errors";
+
+    @Bean
+    public ErrorAttributes errorAttributes() {
+        return new DefaultErrorAttributes() {
+
+            @Override
+            public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+
+                Map<String ,Object> defaultMap = super.getErrorAttributes(webRequest, options);
+
+                Map<String ,Object> errorAttributes = new LinkedHashMap<>();
+                Map<String ,Object> errorAttributesInner = new LinkedHashMap<>();
+
+                errorAttributes.put(KEY_ERROR, errorAttributesInner);
+                errorAttributesInner.put(KEY_MESSAGE, defaultMap.get(KEY_MESSAGE));
+                errorAttributesInner.put(KEY_CODE, defaultMap.get(KEY_STATUS));
+                errorAttributesInner.put(KEY_DESCRIPTION, defaultMap.get(KEY_ERROR));
+
+                return errorAttributes;
+            }
+        };
     }
 }
