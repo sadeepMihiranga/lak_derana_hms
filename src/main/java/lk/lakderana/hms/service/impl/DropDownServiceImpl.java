e82f0a6ba14992b5d02009d58473bd81618e8bf9
sadeep <sadeep@surecore.co>
2021-12-16 23:28:23 +0530

    Added roles and permissions drop downs.

--- src/main/java/lk/lakderana/hms/service/impl/DropDownServiceImpl.java
+++ src/main/java/lk/lakderana/hms/service/impl/DropDownServiceImpl.java
@@ -3,6 +3,8 @@ package lk.lakderana.hms.service.impl;
 import com.google.common.base.Strings;
 import lk.lakderana.hms.dto.DropDownDTO;
 import lk.lakderana.hms.exception.NoRequiredInfoException;
+import lk.lakderana.hms.repository.FunctionRepository;
+import lk.lakderana.hms.repository.RoleRepository;
 import lk.lakderana.hms.service.BranchService;
 import lk.lakderana.hms.service.DepartmentService;
 import lk.lakderana.hms.service.DropDownService;
@@ -18,14 +20,23 @@ public class DropDownServiceImpl implements DropDownService {
 
     private static final String BRANCHES = "BRANCH";
     private static final String DEPARTMENTS = "DEPART";
+    private static final String PERMISSIONS = "PERMIS";
+    private static final String ROLES = "ROLES";
 
     private final BranchService branchService;
     private final DepartmentService departmentService;
 
+    private final FunctionRepository functionRepository;
+    private final RoleRepository roleRepository;
+
     public DropDownServiceImpl(BranchService branchService,
-                               DepartmentService departmentService) {
+                               DepartmentService departmentService,
+                               FunctionRepository functionRepository,
+                               RoleRepository roleRepository) {
         this.branchService = branchService;
         this.departmentService = departmentService;
+        this.functionRepository = functionRepository;
+        this.roleRepository = roleRepository;
     }
 
     @Override
@@ -36,14 +47,27 @@ public class DropDownServiceImpl implements DropDownService {
 
         List<DropDownDTO> downDTOList = new ArrayList<>();
 
-        if(code.equals(BRANCHES)) {
-            branchService.getAllBranches().forEach(branchDTO -> {
-                downDTOList.add(new DropDownDTO(branchDTO.getBranchId().toString(), branchDTO.getMame(), branchDTO.getStatus()));
-            });
-        } else if(code.equals(DEPARTMENTS)) {
-            departmentService.getAllDepartments().forEach(departmentDTO -> {
-                downDTOList.add(new DropDownDTO(departmentDTO.getDepartmentCode(), departmentDTO.getMame(), departmentDTO.getStatus()));
-            });
+        switch (code) {
+            case BRANCHES :
+                branchService.getAllBranches().forEach(branchDTO -> {
+                    downDTOList.add(new DropDownDTO(branchDTO.getBranchId().toString(), branchDTO.getMame(), branchDTO.getStatus()));
+                });
+                break;
+            case DEPARTMENTS :
+                departmentService.getAllDepartments().forEach(departmentDTO -> {
+                    downDTOList.add(new DropDownDTO(departmentDTO.getDepartmentCode(), departmentDTO.getMame(), departmentDTO.getStatus()));
+                });
+                break;
+            case PERMISSIONS :
+                functionRepository.findAll().forEach(tMsFunction -> {
+                    downDTOList.add(new DropDownDTO(tMsFunction.getFuncId(), tMsFunction.getDunsDescription(), tMsFunction.getFuncStatus()));
+                });
+                break;
+            case ROLES :
+                roleRepository.findAll().forEach(tMsRole -> {
+                    downDTOList.add(new DropDownDTO(tMsRole.getRoleId().toString(), tMsRole.getRoleName(), tMsRole.getRoleStatus()));
+                });
+                break;
         }
 
         return downDTOList;
